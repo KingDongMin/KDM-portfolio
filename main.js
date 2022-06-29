2b4a7896b6374bc35d3a0daedaf07251f0654f01
RingDingDongMin <92971320+KingDongMin@users.noreply.github.com>
2022-06-29 13:39:57 +0900

    HTML/CSS/JS : about
    - about me content 추가 (HTML 구성)
    - about me 스타일링
    - skills에 대한 숙련도 애니메이션을 JS를 이용하여 동적으로 표현

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // 엄격모드 적용 : 호이스팅 개선
 "use strict";
 
+// 반응형때 토글버튼으로 메뉴 활성화하기
 const navbarMenuBtn = document.querySelector('.navbar__menuBtn');
 const navbarMenu = document.querySelector('.navbar__menu');
 
@@ -33,4 +34,49 @@ navbarMenu.addEventListener('click', (event)=>{
     scrollTo.scrollIntoView({behavior: 'smooth'});
 
     navbarMenu.classList.toggle('active');
+})
+
+//home
+// contact 버튼으로 contact섹션으로 이동
+const contactBtn = document.querySelector('.profile__contact');
+contactBtn.addEventListener('click', (event)=>{
+    const target = event.target;
+    const link = target.dataset.link
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+})
+
+document.addEventListener('scroll', ()=> {
+    const about = document.querySelector('#about');
+    const skillIcon = document.querySelectorAll('.skill__icon');
+    const skillPercent = document.querySelectorAll('.skill__icon__percent');
+    const aboutArea = about.getBoundingClientRect(); // skills에 해당하는 위치를 가져오기
+    const range = aboutArea.height + aboutArea.top;
+    const htmlwave = document.querySelector('.skill__icon__wave.html');
+    const cssWave = document.querySelector('.skill__icon__wave.css');
+    const jsWave = document.querySelector('.skill__icon__wave.js')
+
+    if(range >= 0 && range <= (aboutArea.height+200) ){
+        htmlwave.classList.add('waveOn');
+        cssWave.classList.add('waveOn');
+        jsWave.classList.add('waveOn');
+
+        for(var i of skillIcon){
+            i.classList.add('active');
+        }
+        for(var i of skillPercent){
+            i.classList.add('active');
+        }
+
+    }else {
+        htmlwave.classList.remove('waveOn');
+        cssWave.classList.remove('waveOn');
+        jsWave.classList.remove('waveOn');
+        for(var i of skillIcon){
+            i.classList.remove('active');
+        }
+        for(var i of skillPercent){
+            i.classList.remove('active');
+        }
+    }
 })
